e1b91d92c1be5f13593ebb095fcea62fee49cf76
hey-ri <kwonhr01@naver.com>
2023-02-06 18:35:25 +0900

    filtering the checked todos (didn't success not yet)

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,20 @@ function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [filterTodoList, setFilterTodoList] = useState(false);
-  const [selectedFilter, setselectedFilter] = useState();
 
   const onItemAdded = (newItem) => {
     setTodoList([...todoList, newItem]);
     console.log({ newItem });
   };
-  const onFilterChanged = () => {};
+
+  const onFilterChanged = (ch) => {
+    if (ch == 'completed') {
+      setTodoList((todos) => todos.filter((todo) => todo.checked === true));
+    } else if (ch == 'incompleted') {
+      setTodoList((todos) => todos.filter((todo) => todo.checked === false));
+    }
+    console.log({ ch, todoList });
+  };
 
   const onRemoveTodo = (id) => {
     setTodoList((todos) => todos.filter((todo) => todo.id !== id));
@@ -30,12 +37,7 @@ function App() {
   return (
     <div className="App">
       <TodoInput onItemAdded={onItemAdded} />
-      <TodoItemList
-        filterTodoList={filterTodoList}
-        todoList={todoList}
-        setTodoList={setTodoList}
-        onRemoveTodo={onRemoveTodo}
-      />
+      <TodoItemList todoList={todoList} setTodoList={setTodoList} onRemoveTodo={onRemoveTodo} />
       <Filter onFilterChanged={onFilterChanged}></Filter>
     </div>
   );
