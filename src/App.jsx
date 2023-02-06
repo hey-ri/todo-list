f57435d4dcceb9cdbd81c2c052bc4a20b847a84b
hey-ri <kwonhr01@naver.com>
2023-02-06 10:51:04 +0900

    change tabWidth setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { TodoInput } from "./TodoInput";
-import { TodoItemList } from "./TodoItemList";
-import Filter from "./Filter";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { TodoInput } from './TodoInput';
+import { TodoItemList } from './TodoItemList';
+import Filter from './Filter';
 
 function App() {
-    // App
-    // @todoList
-    // @filteredTodoList
-    // -- TodoInput  *onItemAdded
-    // -- TodoItemList (map) {@filteredTodoList}
-    //    -- TodoItem {@todo}
-    // -- Filter *onFilterChanged
+  // App
+  // @todoList
+  // @filteredTodoList
+  // -- TodoInput  *onItemAdded
+  // -- TodoItemList (map) {@filteredTodoList}
+  //    -- TodoItem {@todo}
+  // -- Filter *onFilterChanged
 
-    const [todoList, setTodoList] = useState([]);
-    const [filterTodoList, setFilterTodoList] = useState(false);
-    const [selectedFilter, setselectedFilter] = useState();
-    const onItemAdded = (newItem) => {
-        setTodoList([...todoList, newItem]);
-        console.log({ newItem });
-    };
-    const onFilterChanged = () => {};
+  const [todoList, setTodoList] = useState([]);
+  const [filterTodoList, setFilterTodoList] = useState(false);
+  const [selectedFilter, setselectedFilter] = useState();
+  const onItemAdded = (newItem) => {
+    setTodoList([...todoList, newItem]);
+    console.log({ newItem });
+  };
+  const onFilterChanged = () => {};
 
-    const onRemoveTodo = (text) => {
-        setTodoList((todos) => todos.filter((todo) => todo.text !== text));
-        console.log(todoList);
-    };
+  const onRemoveTodo = (text) => {
+    setTodoList((todos) => todos.filter((todo) => todo.text !== text));
+  };
 
-    return (
-        <div className="App">
-            <TodoInput onItemAdded={onItemAdded} />
-            <TodoItemList filterTodoList={filterTodoList} todoList={todoList} onRemoveTodo={onRemoveTodo} />
-            <Filter onFilterChanged={onFilterChanged}></Filter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <TodoInput onItemAdded={onItemAdded} />
+      <TodoItemList filterTodoList={filterTodoList} todoList={todoList} onRemoveTodo={onRemoveTodo} />
+      <Filter onFilterChanged={onFilterChanged}></Filter>
+    </div>
+  );
 }
 
 export default App;
--- src/TodoItemList.jsx
+++ src/TodoItemList.jsx
@@ -1,20 +1,21 @@
 export function TodoItemList({ filterTodoList, todoList, onRemoveTodo }) {
-    return (
-        <div>
-            <div className="todoItemList">
-                {todoList.map((todo, index) => (
-                    <div className="todoItem" key={index}>
-                        {todo.text}
-                        <button
-                            onClick={() => {
-                                onRemoveTodo(todo.text);
-                            }}
-                        >
-                            삭제
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="todoItemList">
+        {todoList.map((todo, index) => (
+          <div className="todoItem" key={index}>
+            {todo.text}
+            <button
+              onClick={() => {
+                onRemoveTodo(todo.text);
+                console.log(todo.text);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
