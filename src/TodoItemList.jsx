6c74f2eaff5a47cddc32eed8fe6c82e08fe40fef
hey-ri <kwonhr01@naver.com>
2023-02-07 11:01:36 +0900

    create TodoItem component

--- src/TodoItemList.jsx
+++ src/TodoItemList.jsx
@@ -1,31 +1,11 @@
-import styles from './TodoItemList.module.css';
+import { TodoItem } from './TodoItem';
 
 export function TodoItemList({ items, onRemoveTodo, onTodoChanged }) {
-  const onCheckedTodo = (todo, checked) => {
-    todo.checked = checked;
-    onTodoChanged(todo);
-    // setTodoList(todoList.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)));
-
-    console.log(items);
-  };
-
   return (
     <div>
       <div className="todoItemList">
         {items.map((todo, index) => (
-          <div key={index}>
-            <input type="checkbox" checked={todo.checked} onChange={(e) => onCheckedTodo(todo, e.target.checked)} />
-            <span className={todo.checked === true ? styles.complete : styles.incomplete}>
-              {todo.text}
-              <button
-                onClick={() => {
-                  onRemoveTodo(todo.id);
-                }}
-              >
-                🗑
-              </button>
-            </span>
-          </div>
+          <TodoItem index={index} todo={todo} onRemoveTodo={onRemoveTodo} onTodoChanged={onTodoChanged} />
         ))}
       </div>
     </div>
