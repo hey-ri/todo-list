f2782a373864f3e546326e6c8d786f1ec3b2edbe
hey-ri <kwonhr01@naver.com>
2023-02-08 15:24:45 +0900

    set focus to input field

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -10,9 +10,6 @@ export function TodoInput({ onItemAdded }) {
     onItemAdded({ text: e.target.task.value, id: uuid(), checked: false });
     console.log(e.target.task.value);
     e.target.task.value = '';
-  };
-
-  const onAdd = () => {
     addRef.current.focus();
   };
 
@@ -21,8 +18,14 @@ export function TodoInput({ onItemAdded }) {
       <form onSubmit={onSubmit}>
         <fieldset>
           <label>할 일:</label>
-          <input name="task" type="text" placeholder="할 일을 입력하세요" className={styles.todo_input_field} />
-          <button type="submit" onClick={onAdd} className={styles.add_btn} ref={addRef}>
+          <input
+            name="task"
+            type="text"
+            placeholder="할 일을 입력하세요"
+            className={styles.todo_input_field}
+            ref={addRef}
+          />
+          <button type="submit" className={styles.add_btn}>
             +
           </button>
         </fieldset>
