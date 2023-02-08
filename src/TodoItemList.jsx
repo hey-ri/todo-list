e3f468aae0e144064b1fc6e807ff2da07cce1bfb
hey-ri <kwonhr01@naver.com>
2023-02-08 13:57:17 +0900

    Add description to differentiate fields

--- src/TodoItemList.jsx
+++ src/TodoItemList.jsx
@@ -3,6 +3,8 @@ import { TodoItem } from './TodoItem';
 export function TodoItemList({ items, onTodoRemoved, onTodoChanged }) {
   return (
     <div className="todo_item_list" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <p style={{ textAlign: 'center', paddingBottom: '.5rem' }}>오늘의 할 일</p>
+      {items.length === 0 && <p style={{ color: 'gray', fontSize: '.9rem' }}>&lt; 할 일이 없습니다 &gt;</p>}
       {items.map((todo, index) => (
         <TodoItem key={index} todo={todo} onTodoRemoved={onTodoRemoved} onTodoChanged={onTodoChanged} />
       ))}
