a2131517b859c71c85d4d013051edfcb44e8d989
hey-ri <kwonhr01@naver.com>
2023-02-08 13:48:44 +0900

    enter a todo and add a todo through the + button

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -1,12 +1,19 @@
+import { useRef } from 'react';
 import uuid from 'react-uuid';
 import styles from './TodoInput.module.css';
 
 export function TodoInput({ onItemAdded }) {
+  const addRef = useRef(null);
+
   const onSubmit = (e) => {
     e.preventDefault();
     onItemAdded({ text: e.target.task.value, id: uuid(), checked: false });
     console.log(e.target.task.value);
     e.target.task.value = '';
+  };
+
+  const onAdd = () => {
+    addRef.current.focus();
   };
 
   return (
@@ -15,6 +22,9 @@ export function TodoInput({ onItemAdded }) {
         <fieldset>
           <label>할 일:</label>
           <input name="task" type="text" placeholder="할 일을 입력하세요" className={styles.todo_input_field} />
+          <button type="submit" onClick={onAdd} className={styles.add_btn} ref={addRef}>
+            +
+          </button>
         </fieldset>
       </form>
     </div>
